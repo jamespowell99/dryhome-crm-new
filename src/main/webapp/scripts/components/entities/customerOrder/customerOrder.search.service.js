aabf1844f26433a4dfb8866249ac0c362f9ef637
James Powell <James.powell@camelotgroup.co.uk>
2016-01-04 21:51:32 +0000

    Revert "Revert "Revert "adding customer entity"""
    This reverts commit b506b4949ceb1f36b1ccf622d8861db54c37a9e4.

--- src/main/webapp/scripts/components/entities/customerOrder/customerOrder.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('dryhomecrmApp')
-    .factory('CustomerOrderSearch', function ($resource) {
-        return $resource('api/_search/customerOrders/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
